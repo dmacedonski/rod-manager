5ea685c0ebda27e8ff341f16276e9c102aab3e45
Damian Macedoński <dmacedonski@gmail.com>
2022-11-19 18:30:27 +0100

    Dodano klasę reprezentującą działkę.

---
+++ RodManager/Data/Plot.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RodManager.Data;
+
+/// <summary>
+///     Reprezentuje działkę.
+/// </summary>
+public class Plot
+{
+    /// <summary>
+    ///     Identyfikator działki.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    ///     Numer działki.
+    /// </summary>
+    [Required(ErrorMessage = "Numer działki jest wymagany.")]
+    public string Number { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Rozmiar działki w metrach kwadratowych.
+    /// </summary>
+    [Required(ErrorMessage = "Wielkość działki jest wymagana.")]
+    public decimal AreaSize { get; set; }
+
+    /// <summary>
+    ///     Czy na terenie działki znajduje się końcowa skrzynka rozdzielcza.
+    /// </summary>
+    public bool HasFinalDistributionBox { get; set; }
+
+    /// <summary>
+    ///     Numer końcowej skrzynki rozdzielczej, do której podłączona jest działka.
+    /// </summary>
+    public string? FinalDistributionBoxNumber { get; set; }
+
+    /// <summary>
+    ///     Numer magistralnej skrzynki rozdzielczej, do której podłączona jest działka.
+    /// </summary>
+    public string? MasterBoxNumber { get; set; }
+
+    /// <summary>
+    ///     Numer licznika prądu.
+    /// </summary>
+    public string? ElectricityMeterNumber { get; set; }
+
+    /// <summary>
+    ///     Numer licznika wody.
+    /// </summary>
+    public string? WaterMeterNumber { get; set; }
+}
